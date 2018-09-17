f32447ff0ac4fc53e2a83c89f8c4ea5bfb6559c3
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-09-17 15:35:58 -0400

    init commit

---
+++ src/Canvas.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Canvas extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <canvas></canvas>
+            </div>
+        )
+    }
+
+}
+
+export default Canvas
