3c1624c1b3c434a5968a8f63107963aaeff5e269
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-09-18 20:18:22 -0400

    add structure to files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Canvas from './Canvas'
+import './assets/css/App.css';
+import Canvas from './components/Canvas'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
