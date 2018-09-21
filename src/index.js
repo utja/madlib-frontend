2ead086f113725dfc122075632553c64972e583c
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-09-21 15:54:23 -0400

    add redux devtools extension

--- src/index.js
+++ src/index.js
@@ -6,5 +6,10 @@ import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './store'
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
+
+  registerServiceWorker();
--- src/store.js
+++ src/store.js
@@ -1,14 +1,12 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers/rootReducer';
 import thunk from 'redux-thunk';
 
 // const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()   
-  )
+  composeWithDevTools(applyMiddleware(thunk))
 )
 
 export default store;
