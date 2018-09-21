b1f2079c156242d5719edd94bcaf25fa2e0e7c3b
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-09-21 17:26:33 -0400

    add loginform component

---
+++ src/components/LoginForm.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { withRouter, Redirect } from 'react-router-dom'
+
+class LoginForm extends React.Component{
+  state = {
+    username: '',
+    password: ''
+  }
+
+  render(){
+    return(
+      <div>Login</div>
+    )
+  }
+
+}
+
+const mapStateToProps = state => {
+
+
+}
+
+const mapDispatchToProps = dispatch => {
+
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForm)
