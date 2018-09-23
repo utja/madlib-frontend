aa7507b5210f356acb14e31acedd4423e94c6199
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-09-23 01:22:17 -0400

    take out console log

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,5 +1,5 @@
 export const createUser = userData => {
-  const { username, password, firstName, lastName } = userData
+  const { username, password, passwordConfirmation, firstName, lastName } = userData
   return dispatch => {
     dispatch({type: 'AUTHENTICATING_USER'})
     fetch(`${process.env.REACT_APP_API_ENDPOINT}/api/v1/users`, {
@@ -12,6 +12,7 @@ export const createUser = userData => {
         user: {
           username: username,
           password: password,
+          password_confirmation: passwordConfirmation,
           first_name: firstName,
           last_name: lastName
         }
@@ -24,19 +25,11 @@ export const createUser = userData => {
         throw response
       }
     })
-    
-    // .then(response => {
-    //   // debugger
-    //   if (response.ok) {
-    //     return response.json()
-    //   } else {
-    //     throw response
-    //   }
-    // })
-    // .then(JSONResponse => {
-    //   console.log(JSONResponse)
-    // })
-    // .catch(response => response.json().then(error => dispatch({type: 'FAILED_LOGIN', payload: error})))
+    .then(JSONResponse => {
+      localStorage.setItem('jwt', JSONResponse.jwt)
+      dispatch({ type: 'SET_CURRENT_USER', payload: JSONResponse.user })
+    })
+    .catch(response => response.json().then(error => dispatch({type: 'FAILED_LOGIN', payload: error})))
   }
 }
 
