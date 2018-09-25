e5caa5efe9050872d23b5c59c91db467ee19e561
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-09-25 14:01:53 -0400

    add drawing components

---
+++ src/containers/NewDrawingContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Story from '../components/story/Story'
+
+class NewDrawingContainer extends React.Component {
+
+  render(){
+    return(
+      <div>
+        <Story/>
+      </div>
+    )
+  }
+}
+
+export default NewDrawingContainer
