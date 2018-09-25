e517381e19ee590f12d9d71462c59fca050a2419
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-09-25 18:54:21 -0400

    add canvas

--- src/containers/NewDrawingContainer.js
+++ src/containers/NewDrawingContainer.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Story from '../components/story/Story'
-
+import Canvas from '../components/drawing/Canvas'
+import Grid from '@material-ui/core/Grid';
 class NewDrawingContainer extends React.Component {
 
   render(){
     return(
-      <div>
-        <Story/>
-      </div>
+      <Grid container className="new-drawing-container">
+        {/* <Story/> */}
+        <Canvas />
+      </Grid>
     )
   }
 }
