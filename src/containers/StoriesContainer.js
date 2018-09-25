9cb89bcdf03c88835f30d213c0904038869852eb
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-09-25 14:01:37 -0400

    remove card form button

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
+import { compose } from 'redux'
 import StoriesList from '../components/story/StoriesList'
 import Story from '../components/story/Story'
 import { getStories } from '../actions/story'
 // import InteractiveList from '../MaterialList'
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button'
+import CardActions from '@material-ui/core/CardActions';
+import Card from '@material-ui/core/Card';
+import PaletteIcon from '@material-ui/icons/Palette'
+import { Link } from 'react-router-dom'
+import { withStyles } from '@material-ui/core/styles';
+
 
 // storiesContainer will render when user goes to /stories to see all stories
 // this page will have a storyList component that will display story items (based on ratings or by newest?)
 // this page will also have an individual story that will display when clicked on from the storyList
+
+const styles = {
+  button: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  }
+}
 class StoriesContainer extends React.Component{
 
   componentDidMount(){
@@ -26,8 +41,26 @@ class StoriesContainer extends React.Component{
         // </div>
       <Grid className="stories-container" container justify="space-around">
         <StoriesList stories={this.props.stories.stories}/>
-        {this.props.stories.selectedStory ? <Story /> : null}
+        <Grid item xs={6}>
+
+        {this.props.stories.selectedStory ? 
+        <Fragment>
+        <Story />
+        <Grid item xs={6}>
+
+        {/* <Card >
+          <CardActions> */}
+            <Button className={this.props.classes.button} component={Link} to="/drawings/new" size="small" color="primary" variant="contained">
+              Create Drawing <PaletteIcon/>
+            </Button>
+          {/* </CardActions>
+        </Card> */}
+        </Grid>
+        </Fragment>
+        : null}
         {/* <InteractiveList /> */}
+        
+        </Grid>
       </Grid>
     )
   }
@@ -47,4 +80,7 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(StoriesContainer)
+export default compose(
+  withStyles(styles),
+  connect(mapStateToProps, mapDispatchToProps)
+  )(StoriesContainer)
