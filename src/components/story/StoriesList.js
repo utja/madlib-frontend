05e1bc648b0a83b6d40f1339b1be8c7cd1f928ee
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-09-27 16:22:01 -0400

    remove duplicate prop item

--- src/components/story/StoriesList.js
+++ src/components/story/StoriesList.js
@@ -5,7 +5,7 @@ import Grid from '@material-ui/core/Grid';
 const StoriesList = props => {
   const mapStoryItems = props.stories.map(story => <StoryItem key={story.id} story={story} />)
   return(
-    <Grid item container direction="column-reverse" xs={4} item>
+    <Grid item container direction="column-reverse" xs={4}>
         {mapStoryItems}
     </Grid>
   )
