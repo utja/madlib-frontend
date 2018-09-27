bd0dad62e63bbf64453b4142c637d43fa4bd63f9
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-09-27 16:32:33 -0400

    connect user

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
+import withAuth from '../hocs/withAuth'
 
 // const images = {
 //   default: 'https://images.pexels.com/photos/419635/notebook-empty-design-paper-419635.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
@@ -12,10 +15,20 @@ class Profile extends React.Component {
 
 
   render(){
+    console.log(this.props)
     return(
       <div>Profile</div>
     )
   }
 }
 
-export default Profile
+const mapStateToProps = state => {
+  return {
+    user: state.user.user
+  }
+}
+
+export default compose(
+  withAuth,
+  connect(mapStateToProps)
+)(Profile)
