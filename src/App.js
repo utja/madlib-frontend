70924bd8e4d10f52de2e98cacf2db244ef272de1
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-09-28 19:00:00 -0400

    clean up comments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-// import { compose } from 'redux'
+import React from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-// import { Route, Switch, Redirect, withRouter } from 'react-router-dom'
-import { Route, Switch, withRouter } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
 import Login from './components/Login'
@@ -12,64 +9,44 @@ import NotFound from './components/NotFound'
 import SignupForm from './components/SignupForm'
 import StoriesContainer from './containers/StoriesContainer'
 import DrawingsContainer from './containers/DrawingsContainer'
-import NewStoryContainer from './containers/NewStoryContainer'
-import StoryContainer from './containers/StoryContainer'
 import './assets/css/App.css';
 import NewDrawingContainer from './containers/NewDrawingContainer';
 import { BrowserRouter as Router } from 'react-router-dom'
+
 const theme = createMuiTheme({
   palette: {
     primary: {
-      // light: will be calculated from palette.primary.main,
       main: '#69F0AE',
-      // dark: will be calculated from palette.primary.main,
-      // contrastText: will be calculated to contrast with palette.primary.main
     },
+    // change secondary colors if necessary
     secondary: {
       light: '#0066ff',
       main: '#0044ff',
-      // dark: will be calculated from palette.secondary.main,
       contrastText: '#ffcc00',
     },
-    // error: will use the default color
   },
 });
 
-// change to functional component
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <MuiThemeProvider theme={theme}>
-          <div className="App">
-            <NavBar />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={SignupForm} />
-              <Route path="/stories" component={StoriesContainer} />} />
-              <Route exact path="/drawings/new" component={NewDrawingContainer} />
-              <Route exact path="/drawings" component={DrawingsContainer} />
-              <Route exact path="/profile" component={Profile} />
-              <Route component={NotFound} />
-            </Switch>
-          </div>
-        </MuiThemeProvider>
-      </Router>
-    );
-  }
+const App = props => {
+  return (
+    <Router>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={SignupForm} />
+            <Route path="/stories" component={StoriesContainer} />} />
+            <Route exact path="/drawings/new" component={NewDrawingContainer} />
+            <Route exact path="/drawings" component={DrawingsContainer} />
+            <Route exact path="/profile" component={Profile} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </MuiThemeProvider>
+    </Router>
+  );
 }
 
-// export default compose(
-//   withRouter(App),
-//   withTheme(theme)
-// )(App);
-// const mapStateToProps = state => {
-//   return {
-//     selectedStory: state.stories.selectedStory
-//   }
-// }
-
-// export default withRouter(App);
 export default App;
-// export default withRouter(connect(mapStateToProps)(App));
