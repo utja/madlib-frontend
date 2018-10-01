457443858f04d3432c2e62fc6b09e029b07771ab
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-10-01 17:18:28 -0400

    change canvas rendered drawings to images

--- src/components/drawing/DrawingCard.js
+++ src/components/drawing/DrawingCard.js
@@ -1,38 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import Grid from '@material-ui/core/Grid';
 
 class DrawingCard extends React.Component {
 
-  //TODO change to image instead of canvas
-  //##############################
-  componentDidMount(){
-    const myCanvas = document.getElementById(`drawing-${this.props.drawing.id}`);
-    const ctx = myCanvas.getContext('2d');
-    const img = new Image();
-    img.onload = function(){
-      ctx.drawImage(img,0,0); // Or at whatever offset you like
-    };
-    img.src = this.props.drawing.data_url;
-  }
-
-  componentDidUpdate(){
-    const myCanvas = document.getElementById(`drawing-${this.props.drawing.id}`);
-    const ctx = myCanvas.getContext('2d');
-    ctx.clearRect(0,0,450,450)
-    const img = new Image();
-    img.onload = function(){
-      ctx.drawImage(img,0,0); // Or at whatever offset you like
-    };
-    img.src = this.props.drawing.data_url;
-  }
-
   render(){
-    
     return(
-      <div>
+      <Grid item>
         <h1 className="cursive">{this.props.drawing.title}</h1>
-        <canvas width="450" height="450" id={`drawing-${this.props.drawing.id}`}></canvas>
-      </div>
+        <img src={this.props.drawing.data_url} alt={this.props.drawing.title}/>
+      </Grid>
 
     )
   }
