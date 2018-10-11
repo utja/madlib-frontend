ca86bf6ceb92f370b48c164bcbd7ee44d17e8e0a
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-10-10 21:34:21 -0400

    paginate drawings

--- src/components/drawing/DrawingsList.js
+++ src/components/drawing/DrawingsList.js
@@ -1,42 +1,65 @@
 import React from 'react'
-// import { connect } from 'react-redux'
-// import StoryItem from './StoryItem'
 import Grid from '@material-ui/core/Grid';
-// import List from '@material-ui/core/List';
 import DrawingItem from './DrawingItem'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TablePaginationActionsWrapped from '../TablePaginationActions'
 
+class DrawingsList extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      page: 0,
+      rowsPerPage: 5,
+    }
+  }
 
-const DrawingsList = props => {
-  // console.log('drawings list props', props)
-  const mapDrawingItems = props.drawings.map(drawing => <DrawingItem key={drawing.id} drawing={drawing}/>)
-  return(
-    // <Grid item >
-    //   <List>
-    //     <h1 className="cursive">Drawings</h1>
-    //     {mapDrawingItems}
-    //   </List>
-    // </Grid>
-    // <Grid item >
-    //   <List>
-    //     <h1 className="cursive">Drawings</h1>
-    //     {mapDrawingItems}
-    //   </List>
-    // </Grid>
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
 
-    // <Grid item container direction="column" xs={4}>
-    //     <h1 className="cursive">Stories</h1>
-    //     <Grid item container direction="column-reverse">
-    //       {mapStoryItems}
-    //     </Grid>
-    //   </Grid>
-
-    <Grid item container direction="column" xs={3}>
-      <h1 className="cursive">Drawings</h1>
-        <Grid item container direction="column-reverse">
-          {mapDrawingItems}
-        </Grid>
-    </Grid>
-  )
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+  
+  render(){
+    const { rowsPerPage, page } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, this.props.drawings.length - page * rowsPerPage);
+    const mapDrawingItems = this.props.drawings.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(drawing => <DrawingItem key={drawing.id} drawing={drawing}/>)
+    return(
+      <Grid item container direction="column" xs={3}>
+        <h1 className="cursive">Drawings</h1>
+          <Grid item container direction="column-reverse">
+            <Table style={{height: '450px'}}>
+              <TableBody>
+                {mapDrawingItems}
+                {emptyRows > 0 && (
+                  <TableRow style={{ height: 48 * emptyRows }}>
+                    <TableCell colSpan={6} />
+                  </TableRow>
+                )}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TablePagination
+                    colSpan={2}
+                    count={this.props.drawings.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onChangePage={this.handleChangePage}
+                    onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                    ActionsComponent={TablePaginationActionsWrapped}
+                  />
+                </TableRow>
+              </TableFooter>
+            </Table>
+          </Grid>
+      </Grid>
+  )}
 }
 
 export default DrawingsList
