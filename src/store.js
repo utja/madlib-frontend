e1fe58e32481b4a8e67eed904cfa6bc6b17c85e5
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-10-11 17:13:12 -0400

    change to logonlyinproduction

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction'
 import rootReducer from './reducers/rootReducer';
 import thunk from 'redux-thunk';
 
