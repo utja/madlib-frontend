ee21b1659a49b8d7a6a39bccab60c8fead446ae1
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-10-11 17:17:54 -0400

    remove composewith devtools

--- src/store.js
+++ src/store.js
@@ -1,12 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction'
+// import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction'
 import rootReducer from './reducers/rootReducer';
 import thunk from 'redux-thunk';
 
 // const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+// const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// )
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  applyMiddleware(thunk)
 )
 
 export default store;
